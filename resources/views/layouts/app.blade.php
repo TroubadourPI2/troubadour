1c2f684417c190e957c43dff7698894a18c98c59
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2025-01-28 18:52:13 -0500

    Ajout axios,tailwind,iconify et layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>@yield('title')</title>
+    <script src="https://code.iconify.design/iconify.min.js"></script>
+
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" type="image/png" href="" />
+</head>
+<body>
+    <header>
+    
+        @yield('header')
+       
+       
+   
+
+    </header>
+
+    <main  >
+        @yield('contenu')
+        
+    
+    </main>
+</body>
+
+
+</html>
