c16b1f1c688672f8f9b87d0424ca24b54469f4d4
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2025-01-28 19:09:04 -0500

    Ajout de prettier pour le formattage de de fichier blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,22 +14,18 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" type="image/png" href="" />
 </head>
+
 <body>
     <header>
-    
+
         @yield('header')
-       
-       
-   
 
     </header>
 
-    <main  >
+    <main>
         @yield('contenu')
-        
-    
+
     </main>
 </body>
-
 
 </html>
