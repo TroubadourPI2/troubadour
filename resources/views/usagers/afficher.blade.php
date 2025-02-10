88bc7dbee3a8f66a39186d18f9b099074b3e23d7
MirolieTheroux <mirolie010@gmail.com>
2025-02-10 15:31:45 -0500

    ajout pages pour le compte (afficher et lieux)

---
+++ resources/views/usagers/afficher.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', 'Compte')
+
+@section('contenu')
+    {{-- //TODO importer la navbar et le footer --}}
+    <div class="flex w-full h-full bg-c2">
+        <div class="flex flex-row">
+            <div class="border-r-4">
+                <button class="font-barlow text-sm text-c1 font-semibold" type="">COMPTE</button>
+            </div>
+            <div>
+                <button class="font-barlow text-sm text-c1 font-semibold" type="">LIEUX</button>
+            </div>
+            <div>
+                <button class="font-barlow text-sm text-c1 font-semibold" type="">ACTIVITÉS</button>
+            </div>
+        </div>
+    </div>
+@endsection
+
+<script src=" {{ asset('js/suppliers/show/showSupplier.js') }} "></script>
