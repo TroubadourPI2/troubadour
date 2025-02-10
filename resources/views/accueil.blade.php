58591b22d9fda740b571013903e59782c4420c9e
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2025-02-10 18:41:39 -0500

    Début de la page de droite

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -6,12 +6,24 @@
 
     <div class="flex w-full h-full bg-c2 max-w-1/2 font-barlow  ">
         <div class="flex flex-col justify-center items-center w-full h-full">
- <img src="{{ asset('images/logo/nom_image.jpg') }}"alt="">
+            <img src="{{ asset('images/Logos/logoC1.svg') }}"alt="Logo de troubadour" class="w-96">
 
-            <span class="text-c1 text-7xl font-barlow font-bold ">TROUBADOUR</span>
-            <span class="text-c1 text-3xl font-barlow  ">Explorez sans limites</span>
+            <span class="text-c1 text-8xl font-barlow font-bold">TROUBADOUR</span>
+            <span class="text-c1 text-4xl font-barlow">Explorez sans limites</span>
         </div>
     </div>
-    <div class="flex w-full h-full bg-c1 max-w-1/2 font-barlow   "></div>
+    <div class="flex w-full h-full bg-c1 max-w-1/2 font-barlow">
+        <div class="flex flex-col justify-start w-full h-full">
+            <navbar class="w-full flex justify-between p-8">
+                <div>
+                    <a class="text-c3 text-2xl px-8 font-barlow">ACCUEIL </a>
+                    <a class="text-c3 text-2xl font-barlow">À PROPOS </a>
+                </div>
+                <div>
+                    <a class=" text-2xl bg-c2 rounded-full p-1.5 px-4 text-c1 font-barlow">CONNEXION </a>
+                </div>
+            </navbar>
+        </div>
+    </div>
 
 @endsection
