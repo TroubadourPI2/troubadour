73231374c16052fb3f972d2420bf0db4ae82da08
MathisGrondin <grondin.mathis@hotmail.com>
2025-02-10 13:24:07 -0500

    test tailwind

---
+++ resources/views/recherche.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Accueil')
+
+@section('contenu')
+
+    <!-- <nav class="bg-red-500 w-full flex">
+        <p>Test</p>
+    </nav> -->
+    <div class="bg-red-500 w-full-flex">
+        <p>test</p>
+    </div>
+    <!-- <div class="flex w-full h-full bg-white ">Troubadour    <span class="iconify" data-icon="mdi:home" data-inline="false"></span></div> -->
+ 
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('accueil');
 });
+
+Route::get('/recherche', function () {
+    return view('recherche');
+});
