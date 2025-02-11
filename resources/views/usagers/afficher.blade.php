9fb7bbcad3528f0affe6877ed7079578592ecc35
MirolieTheroux <mirolie010@gmail.com>
2025-02-11 09:44:54 -0500

    hover sur les boutons

--- resources/views/usagers/afficher.blade.php
+++ resources/views/usagers/afficher.blade.php
@@ -6,11 +6,11 @@
     {{-- //TODO importer la navbar et le footer --}}
     <div class="flex w-full h-full ">
         <div class="flex items-center md:justify-start justify-center md:space-x-2 space-x-4 md:border-b-[3px] border-b-2 border-c1 md:h-10 h-6 w-full hover:">
-            <button class="font-barlow text-sm md:text-2xl text-c1 font-semibold hover:bg-c1 hover:text-c3">COMPTE</button>
+            <button class="font-barlow text-sm md:text-2xl text-c1 font-semibold md:hover:bg-c1 md:hover:text-c3 md:rounded-full md:w-32">COMPTE</button>
             <div class="md:h-6 h-4 md:border-l-[3px] border-l-2 border-c1"></div>
-            <button class="font-barlow text-sm md:text-2xl text-c1 font-semibold">LIEUX</button>
+            <button class="font-barlow text-sm md:text-2xl text-c1 font-semibold md:hover:bg-c1 md:hover:text-c3 md:rounded-full md:w-32">LIEUX</button>
             <div class="md:h-6 h-4 md:border-l-[3px] border-l-2 border-c1"></div>
-            <button class="font-barlow text-sm md:text-2xl text-c1 font-semibold">ACTIVITÉS</button>
+            <button class="font-barlow text-sm md:text-2xl text-c1 font-semibold md:hover:bg-c1 md:hover:text-c3 md:rounded-full md:w-32">ACTIVITÉS</button>
         </div>
         {{-- //TODO Importer les composants selon le menu choisi --}}
     </div>
