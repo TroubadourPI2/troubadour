a55bdda7409ff085708a83edfcaf3ba08c895df5
MathisGrondin <122141558+MathisGrondin@users.noreply.github.com>
2025-02-13 17:38:00 -0500

    Update RegionAdministrativeSeeder.php
    Juste modifié Mauricie par Mauricie et Centre-du-Québec

--- database/seeders/RegionAdministrativeSeeder.php
+++ database/seeders/RegionAdministrativeSeeder.php
@@ -13,7 +13,7 @@ class RegionAdministrativeSeeder extends Seeder
     public function run(): void
     {
         DB::table('RegionAdministrative')->insert([
-            ['id' => 1, 'nom' => 'Mauricie','actif' => true, 'provinceId' => 2, 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 1, 'nom' => 'Mauricie et Centre-du-Québec','actif' => true, 'provinceId' => 2, 'created_at' => now(), 'updated_at' => now()],
             ['id' => 2, 'nom' => 'Montréal','actif' => true, 'provinceId' => 2, 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
