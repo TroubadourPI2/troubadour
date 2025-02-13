d227d705ebc40c6325dbf6c771f7d19ce04aab40
MirolieTheroux <mirolie010@gmail.com>
2025-02-13 11:20:10 -0500

    carte pour web/tablette

--- resources/views/usagers/afficher.blade.php
+++ resources/views/usagers/afficher.blade.php
@@ -5,12 +5,12 @@
 @section('contenu')
     {{-- //TODO importer la navbar et le footer --}}
     <div class="flex flex-col w-full h-full ">
-        <div class="flex items-center md:justify-start text-c1 justify-center md:space-x-2 space-x-4 md:border-b-[3px] border-b-2 border-c1 md:h-10 h-6 w-full ">
-            <button class="text-sm md:text-2xl font-semibold md:hover:bg-c1 md:hover:text-c3 md:rounded-full md:w-32">COMPTE</button>
-            <div class="md:h-6 h-4 md:border-l-[3px] border-l-2 border-c1"></div>
-            <button class="text-sm md:text-2xl font-semibold md:hover:bg-c1 md:hover:text-c3 md:rounded-full md:w-32">LIEUX</button>
-            <div class="md:h-6 h-4 md:border-l-[3px] border-l-2 border-c1"></div>
-            <button class="text-sm md:text-2xl font-semibold md:hover:bg-c1 md:hover:text-c3 md:rounded-full md:w-32">ACTIVITÉS</button>
+        <div class="flex items-center sm:justify-start text-c1 justify-center md:space-x-2 space-x-4 sm:border-b-[3px] border-b-2 border-c1 sm:h-10 h-6 w-full ">
+            <button class="text-sm sm:text-2xl font-semibold sm:hover:bg-c1 sm:hover:text-c3 sm:rounded-full sm:w-32">COMPTE</button>
+            <div class="md:h-6 h-4 sm:border-l-[3px] border-l-2 border-c1"></div>
+            <button class="text-sm sm:text-2xl font-semibold sm:hover:bg-c1 sm:hover:text-c3 sm:rounded-full sm:w-32">LIEUX</button>
+            <div class="md:h-6 h-4 sm:border-l-[3px] border-l-2 border-c1"></div>
+            <button class="text-sm sm:text-2xl font-semibold sm:hover:bg-c1 sm:hover:text-c3 sm:rounded-full sm:w-32">ACTIVITÉS</button>
         </div>
         {{-- //TODO Importer les composants selon le menu choisi --}} 
         <div>@include('usagers.composants.afficherLieux')</div>
--- resources/views/usagers/composants/afficherLieux.blade.php
+++ resources/views/usagers/composants/afficherLieux.blade.php
@@ -1,46 +1,84 @@
 <button
-    class="flex items-center text-sm md:text-xl border-c1 border-2 rounded-full md:w-36 w-[80px] text-c1 font-medium my-3">
-    <span class="iconify text-c1 md:size-8 size-4 md:mr-2" data-icon="ion:add" data-inline="false"></span>
+    class="flex items-center text-sm sm:text-xl border-c1 border-2 rounded-full sm:w-36 w-[80px] text-c1 font-semibold my-3">
+    <span class="iconify text-c1 sm:size-8 size-4 sm:mr-2 font-semibold" data-icon="ion:add" data-inline="false"></span>
     AJOUTER
 </button>
 {{-- //TODO Afficher avec la BD si pas de lieux afficher "Aucun lieu d'enregistrer" --}}
-{{-- //* Afficher seulement en carte pour mobile --}}
-<div class="px-[10vw] sm:px-[5vw] md:px-[15vw] text-c1 items-center align-middle flex flex-row cursor-pointer group [perspective:1000px] sm:hidden">
-    <div id="carteLieuxMobile" class="relative max-w-sm w-full min-h-[60vh] sm:min-h-[50vh] h-auto rounded-[3vw] shadow-lg transition-transform duration-500 [transform-style:preserve-3d] ">
-        <div id="carteLieuxMobileDevant"
-            class="absolute inset-0 bg-c3 overflow-hidden shadow-lg rounded-[3vw] [backface-visibility:hidden]">
-            <img class="w-full h-[50vh] object-cover" src="{{ asset('Images/lieux/borealis.jpg') }}" alt="Boréalis">
-            <div class="px-6 py-4 flex justify-center items-center">
-                <div class="font-semibold mb-2 uppercase">Musée Boréalis</div>
-            </div>
+
+{{-- //* Affichage mobile < 640px --}}
+<!-- Carte lieu pour mobile -->
+<div class="sm:hidden flex flex-col items-center text-c1 rounded-lg shadow-sm">
+    <div id="carteLieuxMobile"
+        class="relative w-full min-h-[36vh] h-auto rounded-lg shadow-lg transition-transform duration-500 [transform-style:preserve-3d]">
+
+        <div id="carteLieuxMobileDevant" class="absolute bg-c3 inset-0 rounded-lg shadow-lg [backface-visibility:hidden]">
+            <img class="object-cover w-full h-72 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                src="{{ asset('Images/lieux/borealis.jpg') }}" alt="Musée Boréalis">
+            <h5 class="mb-2 text-2xl font-semibold uppercase p-2 text-center">Musée Boréalis</h5>
         </div>
+
         <div id="carteLieuxMobileDerriere"
-            class="absolute inset-0 bg-c3 overflow-hidden shadow-lg rounded-[3vw] [transform:rotateY(180deg)] [backface-visibility:hidden] font-semibold">
-            <div class="px-6 py-4 flex flex-col justify-between h-full">
-                <div class="mb-2  ">
-                    <div class="uppercase underline">Description</div>
+            class="absolute inset-0 bg-c3 rounded-lg shadow-lg [transform:rotateY(180deg)] [backface-visibility:hidden] ">
+            <div class="p-4 flex flex-col justify-between h-full">
+                <div class="mb-2">
+                    <div class="uppercase underline text-lg font-semibold">Description</div>
                     <div>Musée du Patrimoine retraçant l'histoire de l'industrie locale du papier et proposant un
                         atelier de fabrication de papier.</div>
                 </div>
                 <div>
-                    <div class="uppercase underline">Coordonnées</div>
+                    <div class="uppercase underline text-lg font-semibold">Coordonnées</div>
                     <div class="flex flex-col">
                         <span>200 Av. des Draveurs</span>
                         <span>Trois-Rivières, QC, G9A 0B6</span>
-                        <span><a href="http://www.borealis3r.ca" class="text-blue-500 underline"
-                                target="_blank">www.borealis3r.ca</a></span>
+                        <span>www.borealis3r.ca</span>
                         <span>(819) 372-4633</span>
                     </div>
                 </div>
-                <div class="flex justify-end space-x-3 mb-2">
-                    <button><span class="iconify text-c1 size-5" data-icon="ion:trash-outline"
+                <div class="flex justify-end space-x-3 mt-3">
+                    <button><span class="iconify text-ci size-6" data-icon="ion:trash-outline"
                             data-inline="false"></span></button>
-                    <button><span class="iconify text-c1 size-5" data-icon="ep:edit"
+                    <button><span class="iconify text-ci size-6" data-icon="ep:edit"
                             data-inline="false"></span></button>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<!-- Carte lieu pour web/tablette  -->
+<div
+    class="flex flex-col sm:flex-row text-c1 rounded-lg shadow-sm bg-white w-full max-w-4xl mx-auto my-4 hidden sm:flex h-[500px]"> <!-- Ajout d'une hauteur maximale -->
+    <div class="w-full sm:w-1/2 rounded-l-lg h-full">
+        <img class="object-cover w-full h-full rounded-l-lg" src="{{ asset('Images/lieux/borealis.jpg') }}"
+            alt="Musée Boréalis">
+    </div>
+
+    <div class="w-full sm:w-1/2 p-4 flex flex-col h-full gap-y-[30px]"> 
+        <h5 class="text-xl sm:text-3xl font-semibold uppercase mb-2">Musée Boréalis</h5>
+
+        <div>
+            <div class="uppercase underline text-2xl font-semibold">Description</div>
+            <div class="text-xl">Musée du Patrimoine retraçant l'histoire de l'industrie locale du papier et proposant
+                un atelier de fabrication de papier.</div>
+        </div>
+
+        <div>
+            <div class="uppercase underline font-semibold text-2xl">Coordonnées</div>
+            <div class="flex flex-col text-xl">
+                <span>200 Av. des Draveurs</span>
+                <span>Trois-Rivières, QC, G9A 0B6</span>
+                <span>www.borealis3r.ca</span>
+                <span>(819) 372-4633</span>
+            </div>
+        </div>
+
+        <div class="flex justify-end space-x-3 mt-auto">
+            <button><span class="iconify text-ci size-8" data-icon="ion:trash-outline"
+                    data-inline="false"></span></button>
+            <button><span class="iconify text-ci size-8" data-icon="ep:edit" data-inline="false"></span></button>
+        </div>
+    </div>
+</div>
+
 
 
