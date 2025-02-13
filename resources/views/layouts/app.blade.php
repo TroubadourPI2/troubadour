4e1de076c018120b137fed34b9194439a626f893
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2025-02-13 16:29:59 -0500

    Ajout de style sur les boutons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,18 +29,19 @@
                 <img src="{{ asset('/Images/Logos/logoC1.svg') }}" class=" w-20 xl:w-24" alt="Logo Troubadour">
                 <span class="text-c1 uppercase text-4xl 2xl:text-5xl font-barlow font-bold">troubadour</span>
             </div>
-            <div class="flex w-full justify-end gap-x-4 items-center">
+            <div class="flex w-full justify-end items-center gap-x-2">
                      {{-- TODO remplacer par les liens quand les pages seronts faites--}}
-                <a class="text-c1 uppercase text-lg 2xl:text-3xl font-barlow ">Attraits</a>
+                <a class="text-c1 uppercase text-lg 2xl:text-3xl font-barlow cursor-pointer hover:bg-c3 px-2 py-1 rounded-full transition  ">Attraits</a>
                 <div class="border-r h-12 border-c1 rounded "></div>
-                <a class="text-c1 uppercase text-lg 2xl:text-3xl font-barlow ">À propos</a>
+                <a class="text-c1 uppercase text-lg 2xl:text-3xl font-barlow cursor-pointer hover:bg-c3 px-2 py-1 rounded-full transition   ">À propos</a>
                 <div class="border-r h-12 border-c1 rounded "></div>
-                <a class="text-c1 uppercase text-lg 2xl:text-3xl font-barlow ">compte</a>
+                <a class="text-c1 uppercase text-lg 2xl:text-3xl font-barlow cursor-pointer hover:bg-c3 px-2 py-1  rounded-full transition   ">compte</a>
                 <div class="border-r h-12 border-c1 rounded "></div>
-                {{-- TODO remplacer par un bouton ou un form en fonction de ce qui a faire mais garder même CSS --}}
-                <a class="text-c1 uppercase text-lg 2xl:text-3xl font-barlow ">déconnexion</a>
+               {{--  TODO remplacer par un bouton ou un form en fonction de ce qui a faire mais garder même CSS
+                TODO Ajouter en fonction de si la personne est connecté ou non l'affichage du bouton connexion deconnexion --}}
+                <a class="text-c1 uppercase text-lg 2xl:text-3xl font-barlow cursor-pointer hover:bg-c3 px-2 py-1  rounded-full transition  ">déconnexion</a>
                 <div class="border-r h-12 border-c1 rounded "></div>
-                <span class="iconify size-6 2xl:size-8" data-icon="mdi:search" data-inline="false"></span>
+                <span class="iconify size-9 2xl:size-10 cursor-pointer hover:bg-c3 px-2 py-1   rounded-full transition " data-icon="mdi:search" data-inline="false"></span>
             </div>
         </div>
         {{-- Menu Mobile --}}
