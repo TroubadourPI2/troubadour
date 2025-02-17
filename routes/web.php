425211a05d43c18ed2e60e2089bc527f312decac
ICodder <143189731+CharlieFournier@users.noreply.github.com>
2025-02-17 13:58:14 -0500

    push pour update from prod
    enlever un deuxieme appel de LieuxController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\LieuxController;
 use App\Http\Controllers\UsagersController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GeolocalisationController;
