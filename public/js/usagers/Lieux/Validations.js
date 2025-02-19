475ca5d1914eae039026731a8b12cbb2815f86c1
MirolieTheroux <mirolie010@gmail.com>
2025-02-19 15:02:36 -0500

    afficher la page d'ajout quand on soummet le formulaire fonctionne

--- public/js/usagers/Lieux/Validations.js
+++
@@ -1,4 +0,0 @@
-//variable de session si le form est ok ou non 
-//if()
-// affiche la bonne section
-//         supprimer la variable de session
--- resources/views/usagers/afficher.blade.php
+++ resources/views/usagers/afficher.blade.php
@@ -24,26 +24,3 @@
 <script src="{{ asset('js/usagers/Lieux/AfficherLieux.js') }}"></script>
 <script src="{{ asset('js/usagers/Lieux/AfficherAjouterLieux.js') }}"></script>
 
-@if(session('erreurAjouterLieu'))
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const boutonCompte = document.getElementById('boutonCompte');
-        const compte = document.getElementById('compte');
-        compte.classList.add("hidden");
-        boutonCompte.classList.remove("bg-c1", "text-c3");
-        boutonCompte.classList.add("sm:hover:bg-c1", "sm:hover:text-c3");
-        const boutonLieu = document.getElementById('boutonLieu');
-        boutonLieu.classList.add("bg-c1", "text-c3");
-        boutonLieu.classList.remove("sm:hover:bg-c1", "sm:hover:text-c3");
-        const sectionAjouterLieu = document.getElementById('ajouterLieu');
-        sectionAjouterLieu.classList.remove("hidden");
-        sectionAjouterLieu.classList.add("flex");
-        const sectionAfficherLieux = document.getElementById('afficherLieux');
-        sectionAfficherLieux.classList.add("hidden");
-        
-    });
-</script>
-@php
-session()->forget('erreurAjouterLieu');
-@endphp
-@endif
