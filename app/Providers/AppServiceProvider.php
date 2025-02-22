f3b0099f5b38d2e0a0734e0362a02b349780aa7a
MirolieTheroux <mirolie010@gmail.com>
2025-02-22 18:07:25 -0500

    remise du utf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         Blade::directive('role', function ($roles) {
             return "<?php if(auth()->check() && in_array(auth()->user()->role->nom, $roles)): ?>";
         });
