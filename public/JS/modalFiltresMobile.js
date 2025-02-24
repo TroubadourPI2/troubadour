5a70c7a611dac01941275d4e1e3eed6ea82348b9
MathisGrondin <grondin.mathis@hotmail.com>
2025-02-24 11:53:30 -0500

    modif confirm button text sweetalert

--- public/JS/modalFiltresMobile.js
+++ public/JS/modalFiltresMobile.js
@@ -140,13 +140,7 @@ mbBtnFiltres.addEventListener('click', function(){
         showCancelButton: false,
         showConfirmButton: false,
         focusConfirm: false,
-        confirmButtonText: `
-          <i class="fa fa-thumbs-up"></i> Great!
-        `,
         confirmButtonAriaLabel: "Thumbs up, great!",
-        cancelButtonText: `
-          <i class="fa fa-thumbs-down"></i>
-        `,
         cancelButtonAriaLabel: "Thumbs down",
         background: "#fbfbfb",
       });
