4ce3b2657a4031292ac02acf722472149f2bcff6
MirolieTheroux <mirolie010@gmail.com>
2025-02-25 10:03:45 -0500

    test carte

---
+++ public/js/test.js
@@ -0,0 +1,32 @@
+let map;
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("test")
+// Initialize and add the map
+
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
+  
+});
