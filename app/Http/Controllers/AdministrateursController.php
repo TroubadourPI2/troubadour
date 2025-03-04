724819c16fd4ae885c4469d2df4044e7521be174
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2025-03-04 09:37:00 -0500

    pagination fonction backend query

--- app/Http/Controllers/AdministrateursController.php
+++ app/Http/Controllers/AdministrateursController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Http\Requests\UsagerRequest;
+use App\Models\Usager;
 class AdministrateursController extends Controller
 {
     /**
@@ -17,10 +18,24 @@ class AdministrateursController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function UsagersPagination(Request $request)
     {
-        //
+
+            $requete = trim($request->get('recherche'));
+    
+            $usagers = Usager::when($requete, function ($requetePerso) use ($requete) {
+                    $requetePerso->where('nom', 'LIKE', '%' . $requete . '%')
+                                 ->orWhere('prenom', 'LIKE', '%' . $requete . '%')
+                                 ->orWhere('courriel', 'LIKE', '%' . $requete . '%');
+                })
+                ->orderByRaw("role_id = 3 DESC")   
+                ->orderByRaw("statut_id = 3 DESC") 
+                ->paginate(10);
+    
+            return response()->json($usagers);
+  
     }
+    
 
     /**
      * Store a newly created resource in storage.
