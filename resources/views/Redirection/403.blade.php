f01efb0bd73ca4c74818fee439c15404dabec73a
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2025-03-08 15:02:10 -0500

    hover c2 fleche

--- resources/views/Redirection/403.blade.php
+++ resources/views/Redirection/403.blade.php
@@ -6,7 +6,7 @@
     <div class="flex items-center justify-center h-screen overflow-hidden relative font-barlow bg-c3">
 
         <div class="absolute inset-x-0 top-5 z-10 text-center w-fit">
-            <a href="/" class="flex items-center hover:text-c3 transition text-c1">
+            <a href="/" class="flex items-center hover:text-c2 transition text-c1">
                 <span class="iconify text-5xl" data-icon="ion:arrow-undo" data-inline="false"></span>
                 <span class="font-bold text-3xl"> {{ __('retour') }}</span>
             </a>
