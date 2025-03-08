b5ab60f107682a262c52cf9b2c4723c7c6f91e4b
MirolieTheroux <mirolie010@gmail.com>
2025-03-07 22:50:36 -0500

    enlever onglet activités

--- resources/views/admin/afficher.blade.php
+++ resources/views/admin/afficher.blade.php
@@ -26,17 +26,11 @@
                 <span class="hidden sm:inline">{{ __('lieux') }}</span>
             </button>
             <div class="sm:h-6 h-4 sm:border-l-[3px] border-l-2 border-c1 hidden sm:inline"></div>
-            <button id="boutonActivites" data-section="activites"
-                class="boutonMenu text-base px-2 py-2 sm:px-4 sm:py-0 sm:text-xl font-semibold sm:hover:bg-c1 sm:hover:text-c3 rounded-full sm:w-32 uppercase transition">
-                <span class="iconify size-8 sm:hidden" data-icon="mdi:events"></span>
-                <span class="hidden sm:inline">{{ __('activites') }}</span>
-            </button>
         </div>
         {{-- //TODO Importer les composants selon le menu choisi --}}
         <div id="demandes" class="sectionMenu">COMPTE</div>
         <div id="villes" class="sectionMenu hidden">VILLES</div>
         <div id="lieux" class="sectionMenu hidden">@include('admin.composants.GestionLieux')</div>
-        <div id="activites" class="sectionMenu hidden">ACTIVITES</div>
     </div>
 
 @endsection
@@ -54,7 +48,6 @@
             const succesMessage = "{{ __('succesAjout') }}";
             document.getElementById('lieux').classList.remove('hidden');
             document.getElementById('demandes').classList.add('hidden');
-            document.getElementById('activites').classList.add('hidden');
             document.getElementById('villes').classList.add('hidden');
 
             const boutonLieu = document.getElementById('boutonLieu');
@@ -94,7 +87,6 @@
             const succesMessage = "{{ __('succesModifier') }}";
             document.getElementById('lieux').classList.remove('hidden');
             document.getElementById('demandes').classList.add('hidden');
-            document.getElementById('activites').classList.add('hidden');
             document.getElementById('villes').classList.add('hidden');
 
             const boutonLieu = document.getElementById('boutonLieu');
@@ -134,15 +126,15 @@
             const succesMessage = "{{ __('succesModifier') }}";
             document.getElementById('lieux').classList.remove('hidden');
             document.getElementById('demandes').classList.add('hidden');
-            document.getElementById('activites').classList.add('hidden');
+            document.getElementById('villes').classList.add('hidden');
 
             const boutonLieux = document.getElementById('boutonLieu');
             boutonLieu.classList.add("bg-c1", "text-c3");
             boutonLieu.classList.remove("sm:hover:bg-c1", "sm:hover:text-c3");
 
-            const boutonCompte = document.getElementById('boutonCompte');
-            boutonCompte.classList.remove("bg-c1", "text-c3");
-            boutonCompte.classList.add("sm:hover:bg-c1", "sm:hover:text-c3");
+            const boutonDemandes = document.getElementById('boutonDemandes');
+            boutonDemandes.classList.remove("bg-c1", "text-c3");
+            boutonDemandes.classList.add("sm:hover:bg-c1", "sm:hover:text-c3");
 
             const Toast = Swal.mixin({
                 toast: true,
