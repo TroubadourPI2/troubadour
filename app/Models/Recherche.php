735d5e6a2027b1a165eb1234a7f5e8845643c554
MathisGrondin <grondin.mathis@hotmail.com>
2025-03-09 12:29:46 -0400

    ajout au model

--- app/Models/Recherche.php
+++ app/Models/Recherche.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Recherche extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'villeId',
+        'quartierId',
+        'termeRecherche',
+        'nbOccurences',
+    ];
 }
