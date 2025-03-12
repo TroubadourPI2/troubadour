99494fad7fdc66f14788a5b12bd3fbe40ec223c1
Juliana Ferrucho <juliana-ort@hotmail.fr>
2025-03-12 12:29:56 -0400

    correction 1

--- resources/views/usagers/composants/AfficherFavoris.blade.php
+++ resources/views/usagers/composants/AfficherFavoris.blade.php
@@ -1,69 +1,69 @@
 <div class="w-full mb-5">
-    @php
-        $favorisLieuxData = App\Models\LieuFavori::whereIn('lieu_id', $favorisLieux)->get();
-        $aLieuxFavoris = $favorisLieuxData->isNotEmpty();
 
-        $favorisActivitesData = App\Models\ActiviteFavori::whereIn('activite_id', $favorisActivites)
-            ->with('activite')
-            ->get();
-        $aActivitesFavoris = $favorisActivitesData->isNotEmpty();
-    @endphp
-    @if (!$aLieuxFavoris && !$aActivitesFavoris)
+    @if (!$favorisLieux && !$favorisActivites)
         <h3 class="text-center text-c1 font-bold font-barlow text-xl">{!! __('aucunFavori') !!}</h3>
     @else
-        @if ($aLieuxFavoris)
+        @if ($favorisLieux)
             <h1 class="text-2xl font-bold text-c1 uppercase font-barlow underline my-5">{!! __('lieuxFavoris') !!}</h1>
 
             <div dir="ltr">
                 <div class="grid grid-flow-col auto-cols-max gap-x-4 overflow-x-auto snap-x whitespace-nowrap">
 
-                    @foreach ($favorisLieuxData as $favoriL)
-                        <a href="/lieu/zoom/{{ $favoriL->lieu->id }}"
-                            class="w-[300px] h-96 bg-c3 transition shadow-lg rounded-md cursor-pointer relative overflow-hidden border border-transparent hover:border-c1 my-2 flex flex-col justify-end">
-                            <img src="{{ asset($favoriL->lieu->photo_lieu_url) }}"
-                                alt="{{ $favoriL->lieu->nomEtablissement }}"
-                                class="w-full h-96 object-cover absolute inset-0 transition duration-300 ease-in-out">
+                    @foreach ($favorisLieux as $lieuID)
+                        @php
+                            $lieu = \App\Models\Lieu::find($lieuID);
+                        @endphp
+                        @if ($lieu)
+                            <a href="/lieu/zoom/{{ $lieu->id }}"
+                                class="w-[300px] h-96 bg-c3 transition shadow-lg rounded-md cursor-pointer relative overflow-hidden border border-transparent hover:border-c1 my-2 flex flex-col justify-end">
+                                <img src="{{ asset($lieu->photo_lieu_url) }}" alt="{{ $lieu->nomEtablissement }}"
+                                    class="w-full h-96 object-cover absolute inset-0 transition duration-300 ease-in-out">
 
-                            <div class="w-full flex justify-center items-end">
-                                <div class="opacity-90 bg-c1 flex w-full h-16 items-center">
-                                    <span class="text-2xl font-bold text-c3 font-barlow w-full truncate px-4">
-                                        {{ $favoriL->lieu->nomEtablissement }}
-                                    </span>
-                                    <div class="flex gap-x-2 px-4">
-                                        <form action="{{ route('delete.favoris.lieu', ['id' => $favoriL->id]) }}"
-                                            method="POST">
-                                            @csrf
-                                            <input type="hidden" name="id" value="{{ $favoriL->id }}">
-                                            <button type="submit" style="background: none; border: none;">
-                                                <span
-                                                    class="iconify size-10 md:ml-0 lg:ml-0 ml-5 text-c2 sm:ml-0 sm:mr-0 md:mr-0"
-                                                    data-icon="line-md:heart-filled" data-inline="false"></span>
-                                            </button>
-                                        </form>
+                                <div class="w-full flex justify-center items-end">
+                                    <div class="opacity-90 bg-c1 flex w-full h-16 items-center">
+                                        <span class="text-2xl font-bold text-c3 font-barlow w-full truncate px-4">
+                                            {{ $lieu->nomEtablissement }}
+                                        </span>
+                                        <div class="flex gap-x-2 px-4">
+                                            <form action="{{ route('delete.favoris.lieu', ['id' => $lieu->id]) }}"
+                                                method="POST">
+                                                @csrf
+                                                <input type="hidden" name="id" value="{{ $lieu->id }}">
+                                                <button type="submit" style="background: none; border: none;">
+                                                    <span
+                                                        class="iconify size-10 md:ml-0 lg:ml-0 ml-5 text-c2 sm:ml-0 sm:mr-0 md:mr-0"
+                                                        data-icon="line-md:heart-filled" data-inline="false"></span>
+                                                </button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </a>
+                            </a>
+                        @endif
                     @endforeach
                 </div>
             </div>
         @endif
 
-        @if ($aActivitesFavoris)
+        @if ($favorisActivites)
             <h1 class="text-2xl font-bold text-c1 uppercase font-barlow underline my-5">{!! __('activitesFavorites') !!}</h1>
             <div dir="ltr">
                 <div class="grid grid-flow-col auto-cols-max gap-x-4 overflow-x-auto snap-x whitespace-nowrap">
 
-                    @foreach ($favorisActivitesData as $favoriA)
-                        <a href="/activite/zoom/{{ $favoriA->activite->id }}/{{ $favoriA->activite->lieux->first()->id ?? '' }}"
-                            class="activite-carte w-[300px] h-96 bg-c3 transition shadow-lg rounded-md cursor-pointer relative overflow-hidden border border-transparent hover:border-c1 my-2 flex flex-col lg:flex-row justify-end"
-                            data-nom="{{ strtolower($favoriA->activite->nom) }}"
-                            data-lieu-ids="{{ $favoriA->activite->lieu_ids }}"
-                            data-type="{{ $favoriA->activite->typeActivite->id }}"
-                            data-actif="{{ $favoriA->activite->actif }}"
-                            x-data='{
+                    @foreach ($favorisActivites as $activiteID)
+                        @php
+                            $activite = \App\Models\Activite::find($activiteID);
+                        @endphp
+                        @if ($activite)
+                            <a href="/activite/zoom/{{ $activite->id }}/{{ $activite->lieux->first()->id ?? '' }}"
+                                class="activite-carte w-[300px] h-96 bg-c3 transition shadow-lg rounded-md cursor-pointer relative overflow-hidden border border-transparent hover:border-c1 my-2 flex flex-col lg:flex-row justify-end"
+                                data-nom="{{ strtolower($activite->nom) }}"
+                                data-lieu-ids="{{ $activite->lieu_ids }}"
+                                data-type="{{ $activite->typeActivite->id }}"
+                                data-actif="{{ $activite->actif }}"
+                                x-data='{
                         current: 0,
-                        images: {!! $favoriA->activite->photos_json !!},
+                        images: {!! $activite->photos_json !!},
                         interval: null,
                         next() {
                             this.current = (this.current < this.images.length - 1) ? this.current + 1 : 0;
@@ -77,35 +77,37 @@
                             clearInterval(this.interval);
                         }
                     }'
-                            x-init="startCarousel()" @mouseenter="stopCarousel()" @mouseleave="startCarousel()">
-                            <template x-for="(img, index) in images" :key="index">
-                                <img x-show="current === index" :src="img" alt="Photo de l'activité"
-                                    class="w-full h-96 object-cover absolute inset-0 transition duration-300 ease-in-out"
-                                    x-transition:enter="transition transform duration-300"
-                                    x-transition:enter-start="opacity-0 scale-90"
-                                    x-transition:enter-end="opacity-100 scale-100" />
-                            </template>
+                                x-init="startCarousel()" @mouseenter="stopCarousel()" @mouseleave="startCarousel()">
+                                <template x-for="(img, index) in images" :key="index">
+                                    <img x-show="current === index" :src="img" alt="Photo de l'activité"
+                                        class="w-full h-96 object-cover absolute inset-0 transition duration-300 ease-in-out"
+                                        x-transition:enter="transition transform duration-300"
+                                        x-transition:enter-start="opacity-0 scale-90"
+                                        x-transition:enter-end="opacity-100 scale-100" />
+                                </template>
 
-                            <div class="w-full flex justify-center items-end">
-                                <div class="opacity-90 bg-c1 flex w-full h-16 items-center">
-                                    <span class="text-2xl font-bold text-c3 font-barlow w-full truncate px-4">
-                                        {{ $favoriA->activite->nom }}
-                                    </span>
-                                    <div class="ml-auto px-4 flex justify-end">
-                                        <form action="{{ route('delete.favoris.activite', ['id' => $favoriA->id]) }}"
-                                            method="POST">
-                                            @csrf
-                                            <input type="hidden" name="id" value="{{ $favoriA->id }}">
-                                            <button type="submit" style="background: none; border: none;">
-                                                <span
-                                                    class="iconify size-10 md:ml-0 lg:ml-0 ml-5 text-c2 sm:ml-0 sm:mr-0 md:mr-0"
-                                                    data-icon="line-md:heart-filled" data-inline="false"></span>
-                                            </button>
-                                        </form>
+                                <div class="w-full flex justify-center items-end">
+                                    <div class="opacity-90 bg-c1 flex w-full h-16 items-center">
+                                        <span class="text-2xl font-bold text-c3 font-barlow w-full truncate px-4">
+                                            {{ $activite->nom }}
+                                        </span>
+                                        <div class="ml-auto px-4 flex justify-end">
+                                            <form
+                                                action="{{ route('delete.favoris.activite', ['id' => $activite->id]) }}"
+                                                method="POST">
+                                                @csrf
+                                                <input type="hidden" name="id" value="{{ $activite->id }}">
+                                                <button type="submit" style="background: none; border: none;">
+                                                    <span
+                                                        class="iconify size-10 md:ml-0 lg:ml-0 ml-5 text-c2 sm:ml-0 sm:mr-0 md:mr-0"
+                                                        data-icon="line-md:heart-filled" data-inline="false"></span>
+                                                </button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </a>
+                            </a>
+                        @endif
                     @endforeach
                 </div>
             </div>
