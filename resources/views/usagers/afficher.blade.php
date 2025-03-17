344e920e70a7b6eb292d672166cc4b1ead0d3611
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2025-03-17 16:13:55 -0400

    Modification typo

--- resources/views/usagers/afficher.blade.php
+++ resources/views/usagers/afficher.blade.php
@@ -19,8 +19,8 @@
             @endrole
         </div>
         <div id="compte" class="sectionMenu">@include('usagers.composants.AfficherCompte')</div>
-        <div id="lieux" class="sectionMenu hidden">@include('usagers.composants.AfficherLieux')</div>
-        <div id="activites" class="sectionMenu hidden">@include('usagers.composants.afficherActivites')</div>
+        <div id="lieux" class="sectionMenu hidden">@include('usagers.composants.afficherLieux')</div>
+        <div id="activites" class="sectionMenu hidden">@include('usagers.composants.AfficherActivites')</div>
     </div>
 @endsection
 <script src="{{ asset('js/usagers/GestionAffichageMenu.js') }}"></script>
