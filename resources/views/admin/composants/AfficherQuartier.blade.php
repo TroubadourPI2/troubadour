8d723ca8ce8a62b88698fbb08e92f55690ec586a
Mirolie Théroux <143189678+MirolieTheroux@users.noreply.github.com>
2025-03-19 12:52:08 -0400

    Fixup miro (#97) messages derreur
    * empecher obtenir info lieu si l'utilisateur n'Est pas propriétaire du lieu
    
    * rajout throttle
    
    * message derreurs
    
    * npm run format

--- resources/views/admin/composants/AfficherQuartier.blade.php
+++ resources/views/admin/composants/AfficherQuartier.blade.php
@@ -1,37 +1,35 @@
 <div class="flex w-full h-full flex-col" id="afficherQuartiers">
-    
-    <button id="boutonAjouterQuartier" class="rounded-full w-fit items-center uppercase text-lg flex leading-tight border-c1 border-2 text-c1 bg-c2 hover:bg-c3 pr-4">
+
+    <button id="boutonAjouterQuartier"
+        class="rounded-full w-fit items-center uppercase text-lg flex leading-tight border-c1 border-2 text-c1 bg-c2 hover:bg-c3 pr-4">
         <span class="iconify text-c1 sm:size-8 size-4 sm:ml-2 font-semibold" data-icon="ion:add"
-        data-inline="false"></span>
+            data-inline="false"></span>
         {{ __('ajouter') }}
     </button>
     <div class="grid lg:grid-cols-4 md:grid-cols-2" id="affichageQuartiers"></div>
 
-</div>  
+</div>
 
 <div id="ajouterQuartier" class="hidden">@include('admin.composants.AjouterQuartier')</div>
 <div id="modifierQuartier" class="hidden">@include('admin.composants.ModifierQuartier')</div>
 
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        document.getElementById("boutonAjouterQuartier").addEventListener("click", function() {
+            let affichage = document.getElementById("afficherQuartiers");
+            let ajout = document.getElementById("ajouterQuartier");
+            affichage.classList.add("hidden");
+            ajout.classList.remove("hidden");
+        });
 
-    <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            document.getElementById("boutonAjouterQuartier").addEventListener("click", function () {
-                let affichage = document.getElementById("afficherQuartiers"); 
-                let ajout = document.getElementById("ajouterQuartier"); 
+        document.getElementById("afficherQuartiers").addEventListener("click", function(event) {
+            if (event.target.closest(".boutonModifierQuartier")) {
+                let affichage = document.getElementById("afficherQuartiers");
+                let ajout = document.getElementById("modifierQuartier");
                 affichage.classList.add("hidden");
                 ajout.classList.remove("hidden");
-            });
-
-            document.getElementById("afficherQuartiers").addEventListener("click", function (event) {
-                if (event.target.closest(".boutonModifierQuartier")) {
-                    let affichage = document.getElementById("afficherQuartiers");
-                    let ajout = document.getElementById("modifierQuartier");
-                    affichage.classList.add("hidden");
-                    ajout.classList.remove("hidden");
-                }
-            });
-
+            }
         });
-    </script>
 
-
+    });
+</script>
