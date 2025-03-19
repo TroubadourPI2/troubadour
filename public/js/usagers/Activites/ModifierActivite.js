be0d3890656d6b793b84a3a87a1c726498f4e185
Nathan Michel <143432891+MN97490@users.noreply.github.com>
2025-03-19 13:17:36 -0400

    Sweet alert erreur changement/nathan (#99)
    * Fix menu mobile transitions et ajout clickable pour aller vers l'accueil
    
    * Correction visuel swal
    
    * retirer recherche dans menu mobile et desktop

--- public/js/usagers/Activites/ModifierActivite.js
+++ public/js/usagers/Activites/ModifierActivite.js
@@ -181,11 +181,25 @@ document.addEventListener('DOMContentLoaded', () => {
                     
                 })
                 .catch(error => {
-                    Swal.fire(
-                        Lang.get('strings.erreur'), 
-                        error.response.data.message,         
-                        "error"
-                    );
+                    Swal.fire({
+                        icon: 'error',
+                        title: Lang.get('strings.erreur'),
+                        text:  error.response.data.message,  
+                        customClass: {
+                            popup: 'font-barlow text-xl text-c1 bg-c2',
+                            title: 'text-3xl uppercase underline',
+                            confirmButton: 'bg-c1 text-white font-semibold px-4 py-2 uppercase rounded-full transition',
+                        },
+                        didOpen: () => {
+                            const xMarkLeft = document.querySelector('.swal2-x-mark-line-left');
+                            const xMarkRight = document.querySelector('.swal2-x-mark-line-right');
+                    
+                            if (xMarkLeft && xMarkRight) {
+                                xMarkLeft.style.backgroundColor = '#154C51'; 
+                                xMarkRight.style.backgroundColor = '#154C51'; 
+                            }
+                        }
+                    });
                 });
         });
     });
