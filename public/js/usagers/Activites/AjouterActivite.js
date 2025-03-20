aea9f62b0de6277fb231d0f620386ba80d5767f3
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2025-03-20 09:15:30 -0400

    Ajout validations js de type

--- public/js/usagers/Activites/AjouterActivite.js
+++ public/js/usagers/Activites/AjouterActivite.js
@@ -34,6 +34,30 @@ champPhotos.addEventListener('change', function() {
 
     const tailleMax = 2 * 1024 * 1024; 
     for (let i = 0; i < champPhotos.files.length; i++) {
+        if (!(champPhotos.files[i].type=== 'image/jpeg' || champPhotos.files[i].type === 'image/png')){
+            Swal.fire({
+                icon: 'error',
+                title: Lang.get('strings.erreur'),
+                text: Lang.get('validations.photoMime'),
+                customClass: {
+                    popup: 'font-barlow text-xl text-c1 bg-c2',
+                    title: 'text-3xl uppercase underline',
+                    confirmButton: 'bg-c1 text-white font-semibold px-4 py-2 uppercase rounded-full transition',
+                },
+                didOpen: () => {
+                    const xMarkLeft = document.querySelector('.swal2-x-mark-line-left');
+                    const xMarkRight = document.querySelector('.swal2-x-mark-line-right');
+            
+                    if (xMarkLeft && xMarkRight) {
+                        xMarkLeft.style.backgroundColor = '#154C51'; 
+                        xMarkRight.style.backgroundColor = '#154C51'; 
+                    }
+                }
+            });
+            champPhotos.value = '';
+            conteneurPositions.innerHTML = '';
+            return;
+        }
         if (champPhotos.files[i].size > tailleMax) {
             Swal.fire({
                 icon: 'error',
--- public/js/usagers/Activites/ModifierActivite.js
+++ public/js/usagers/Activites/ModifierActivite.js
@@ -249,6 +249,31 @@ document.getElementById('photosModif').addEventListener('change', function() {
     }
     const tailleMax = 2 * 1024 * 1024; 
     for (let i = 0; i < photosModif.files.length; i++) {
+
+        if (!(photosModif.files[i].type=== 'image/jpeg' || photosModif.files[i].type === 'image/png')){
+            Swal.fire({
+                icon: 'error',
+                title: Lang.get('strings.erreur'),
+                text: Lang.get('validations.photoMime'),
+                customClass: {
+                    popup: 'font-barlow text-xl text-c1 bg-c2',
+                    title: 'text-3xl uppercase underline',
+                    confirmButton: 'bg-c1 text-white font-semibold px-4 py-2 uppercase rounded-full transition',
+                },
+                didOpen: () => {
+                    const xMarkLeft = document.querySelector('.swal2-x-mark-line-left');
+                    const xMarkRight = document.querySelector('.swal2-x-mark-line-right');
+            
+                    if (xMarkLeft && xMarkRight) {
+                        xMarkLeft.style.backgroundColor = '#154C51'; 
+                        xMarkRight.style.backgroundColor = '#154C51'; 
+                    }
+                }
+            });
+            photosModif.value = '';
+            conteneurPositions.innerHTML = '';
+            return;
+        }
         if (photosModif.files[i].size > tailleMax) {
             Swal.fire({
                 icon: 'error',
