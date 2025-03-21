dcd185be14fdc15585611101e9aec38de3c74fd6
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2025-03-21 14:47:14 -0400

    Test unitaire okay on doit remplacer dans request par file

--- tests/Unit/AjouterActiviteTest.php
+++ tests/Unit/AjouterActiviteTest.php
@@ -9,19 +9,44 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Requests\ActiviteRequest;
 use App\Models\Activite;
+use App\Models\Usager;
 use Illuminate\Support\Facades\Storage;
 
 
 class AjouterActiviteTest extends TestCase
 {   
     use RefreshDatabase;
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
+    public function testActiviteAvecDonneesValides()
     {
-        $this->assertTrue(true);
         $this->seed();
+
         
+        $donnee = [
+            'nomActivite'         => 'Atelier harmonica',
+            'dateDebut'           => now()->addDay()->toDateString(),
+            'dateFin'             => now()->addDays(2)->toDateString(),
+            'descriptionActivite' => 'Test Lieu',
+            'lieu_id'             => [2],
+            'typeActivite_id'     => 1,
+            'photos' => [
+                [
+                    'files' => UploadedFile::fake()->image('photo.jpg'),
+                    'position' => 1,
+                ]
+            ]
+        ];
+        
+    
+        $validator = Validator::make($donnee, (new ActiviteRequest())->rules());
+
+
+
+        $this->assertFalse($validator->fails(), 'La validation ne devrait pas échouer.');
     }
-}
+    
+
+} 
+
+
+
+
